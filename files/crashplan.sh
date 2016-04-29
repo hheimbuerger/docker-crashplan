957eead9b0fbe6513cad73ba3be152914b528698
Henrik Heimbuerger <henrik@heimbuerger.de>
2016-04-29 22:11:08 +0200

    First attempt at running CrashPlan as a foreground application.

--- files/crashplan.sh
+++ files/crashplan.sh
@@ -12,9 +12,20 @@ term_handler() {
 trap 'kill "$tail_pid"; term_handler' INT QUIT KILL TERM
 
 # experiment with modified JAVA_OPTS from http://www.oracle.com/technetwork/java/javase/clopts-139448.html#gbzrr
-printf 'SRV_JAVA_OPTS="-Dfile.encoding=UTF-8 -Dapp=CrashPlanService -DappBaseName=CrashPlan -Xms20m -Xmx1024m -Dsun.net.inetaddr.ttl=300 -Dnetworkaddress.cache.ttl=300 -Dsun.net.inetaddr.negative.ttl=0 -Dnetworkaddress.cache.negative.ttl=0 -Dc42.native.md5.enabled=false -XX:+HeapDumpOnOutOfMemoryError -XX:+ShowMessageBoxOnError -XX:HeapDumpPath=/var/crashplan/dumps"\nGUI_JAVA_OPTS="-Dfile.encoding=UTF-8 -Dapp=CrashPlanDesktop -DappBaseName=CrashPlan -Xms20m -Xmx512m -Dsun.net.inetaddr.ttl=300 -Dnetworkaddress.cache.ttl=300 -Dsun.net.inetaddr.negative.ttl=0 -Dnetworkaddress.cache.negative.ttl=0 -Dc42.native.md5.enabled=false -XX:+HeapDumpOnOutOfMemoryError -XX:+ShowMessageBoxOnError -XX:HeapDumpPath=/var/crashplan/dumps"' > /var/crashplan/bin/run.conf
+# printf 'SRV_JAVA_OPTS="-Dfile.encoding=UTF-8 -Dapp=CrashPlanService -DappBaseName=CrashPlan -Xms20m -Xmx1024m -Dsun.net.inetaddr.ttl=300 -Dnetworkaddress.cache.ttl=300 -Dsun.net.inetaddr.negative.ttl=0 -Dnetworkaddress.cache.negative.ttl=0 -Dc42.native.md5.enabled=false -XX:+HeapDumpOnOutOfMemoryError -XX:+ShowMessageBoxOnError -XX:HeapDumpPath=/var/crashplan/dumps"\nGUI_JAVA_OPTS="-Dfile.encoding=UTF-8 -Dapp=CrashPlanDesktop -DappBaseName=CrashPlan -Xms20m -Xmx512m -Dsun.net.inetaddr.ttl=300 -Dnetworkaddress.cache.ttl=300 -Dsun.net.inetaddr.negative.ttl=0 -Dnetworkaddress.cache.negative.ttl=0 -Dc42.native.md5.enabled=false -XX:+HeapDumpOnOutOfMemoryError -XX:+ShowMessageBoxOnError -XX:HeapDumpPath=/var/crashplan/dumps"' > /var/crashplan/bin/run.conf
 
-/etc/init.d/crashplan start
+# try to start crashplan as a foreground app
+# /etc/init.d/crashplan start
+TARGETDIR="/usr/local/crashplan"
+SRV_JAVA_OPTS="-Dfile.encoding=UTF-8 -Dapp=CrashPlanService -DappBaseName=CrashPlan -Xms20m -Xmx1024m -Dsun.net.inetaddr.ttl=300 -Dnetworkaddress.cache.ttl=300 -Dsun.net.inetaddr.negative.ttl=0 -Dnetworkaddress.cache.negative.ttl=0 -Dc42.native.md5.enabled=false -XX:+HeapDumpOnOutOfMemoryError -XX:+ShowMessageBoxOnError -XX:HeapDumpPath=/var/crashplan/dumps"
+GUI_JAVA_OPTS="-Dfile.encoding=UTF-8 -Dapp=CrashPlanDesktop -DappBaseName=CrashPlan -Xms20m -Xmx512m -Dsun.net.inetaddr.ttl=300 -Dnetworkaddress.cache.ttl=300 -Dsun.net.inetaddr.negative.ttl=0 -Dnetworkaddress.cache.negative.ttl=0 -Dc42.native.md5.enabled=false -XX:+HeapDumpOnOutOfMemoryError -XX:+ShowMessageBoxOnError -XX:HeapDumpPath=/var/crashplan/dumps"
+FULL_CP="$TARGETDIR/lib/com.backup42.desktop.jar:$TARGETDIR/lang"
+printenv > /var/crashplan/dumps/full_env
+cd $TARGETDIR
+nice -n 19 $JAVACOMMON $SRV_JAVA_OPTS -classpath $FULL_CP com.backup42.service.CPService > $TARGETDIR/log/engine_output.log 2> $TARGETDIR/log/engine_error.log
+
+# THIS SHOULD NOT EVER BE EXECUTED -----------------------------
+touch /var/crashplan/dumps/java_process_died
 
 LOGS_FILES="/var/crashplan/log/service.log.0"
 for file in $LOGS_FILES; do
